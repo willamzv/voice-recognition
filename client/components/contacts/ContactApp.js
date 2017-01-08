dbe6dba043e25058e4621feeb8f1bca312f8f540
Willam Zavala <ricardozv_28@hotmail.com>
2017-01-08 11:43:53 -0700

    fix bug on contactapp.js

--- client/components/contacts/ContactApp.js
+++ client/components/contacts/ContactApp.js
@@ -8,7 +8,6 @@ import { fetchContacts } from '../../actions/actions.js';
 class ContactApp extends React.Component {
   componentDidMount(){
     this.props.dispatch(fetchContacts());
-<<<<<<< HEAD
     artyom.fatality();
     setTimeout(()=>{
       artyom.initialize({
@@ -22,18 +21,6 @@ class ContactApp extends React.Component {
         obeyKeyword: "Nova",
       });
     },2000);
-=======
-    artyom.initialize({
-      lang:"en-US",// A lot of languages are supported. Read the docs !
-      continuous:true,// Artyom will listen forever
-      listen:true, // Start recognizing
-      debug:true, // Show everything in the console
-      speed:1, // talk normally
-      soundex:true,
-      executionKeyword: 'now',
-      obeyKeyword: "Nova",
-    });
->>>>>>> 8752bec05c829f68f834e373c95bcc166e6c99bc
   }
 
   render() {

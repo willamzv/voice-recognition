28e440a842fc1d6f4e276784aad172b13e513670
Willam Zavala <ricardozv_28@hotmail.com>
2017-01-10 02:21:52 -0700

    change artyom option debug to false

--- client/components/Home.js
+++ client/components/Home.js
@@ -17,7 +17,7 @@ class Home extends React.Component{
         lang:"en-US",// A lot of languages are supported. Read the docs !
         continuous:true,// Artyom will listen forever
         listen:true, // Start recognizing
-        debug:true, // Show everything in the console
+        debug:false, // Show everything in the console
         speed:1, // talk normally
         soundex:true,
         executionKeyword: 'now',

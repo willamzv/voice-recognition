c5d5c2f8a9cb9d05f51099c10e9208202c7adf6f
Willam Zavala <ricardozv_28@hotmail.com>
2017-01-09 23:18:03 -0700

    heroku

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-mongoose.connect( 'mongodb://localhost/capstone' );
+
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/capstone';
+mongoose.connect(mongoUri);
 
 var routes = require('./routes/index');
 var auth = require('./routes/auth');
